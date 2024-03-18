f975a93448a85ee9e6cde4372c281f7380136220
Joel-ASantos <joel.alves.santos77@gmail.com>
2024-03-18 11:04:13 -0300

    add

---
+++ aula2/Main.java
@@ -0,0 +1,39 @@
+package aula2;
+
+public class Main {
+	public static void main(String[] args) {
+		System.out.println("Hello, World!");
+		
+		Ola ola = new Ola();
+		Ola ola2 = new Ola();
+		Ola ola3 = new Ola();
+		Ola ola4 = new Ola();
+		Ola ola5 = new Ola();
+		Ola ola6 = new Ola();
+		Ola ola7 = new Ola();
+	
+		Tchau tchau = new Tchau();
+		Tchau tchau2 = new Tchau();
+		Tchau tchau3 = new Tchau();
+		Tchau tchau4 = new Tchau();
+		Tchau tchau5 = new Tchau();
+		Tchau tchau6 = new Tchau();
+		Tchau tchau7 = new Tchau();
+		
+		ola.start();
+		ola2.start();
+		ola3.start();
+		ola4.start();
+		ola5.start();
+		ola6.start();
+		ola7.start();
+		
+		tchau.start();
+		tchau2.start();
+		tchau3.start();
+		tchau4.start();
+		tchau5.start();
+		tchau6.start();
+		tchau7.start();
+	}
+}
